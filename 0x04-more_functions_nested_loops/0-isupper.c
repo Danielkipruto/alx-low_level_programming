15766f276980371940948ce4de78e9639e48c6c8
Danielkipruto <dannierutto60@gmail.com>
2023-06-22 16:04:25 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -16,5 +16,3 @@ int _isupper(int c)
 {
 	return (0);
 }
-
-}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/*
- * Header file for all prototypes
- * used in 0x02-functions_nested_loops
- */
-
 int _putchar(char c);
-
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 #endif
