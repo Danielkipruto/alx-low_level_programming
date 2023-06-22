80535a8f47dc861a0bf45ae8460e704719c737ab
Danielkipruto <dannierutto60@gmail.com>
2023-06-22 16:27:12 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,15 +4,12 @@
 * @c: Variable text
 * Return: Always 0.
 */
+
 int _isupper(int c)
 {
-
-
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 	{
-	return (1);
+		return (1);
 	}
-	else
-{
 	return (0);
 }
