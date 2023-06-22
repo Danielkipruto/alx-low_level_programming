5b313a09c2318fd9806c21d75e63ded0addc12f9
Danielkipruto <dannierutto60@gmail.com>
2023-06-22 12:19:04 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _isupper - returns whether a character's ASCII value is uppercase
  * @c: ASCII character to be checked
- *
- * Return: 1 if it is uppercase and 0 if it isnt
+ * Return: 1 if it is uppercase and 0 for any else
  */
 int _isupper(int c)
 {
-  if (c >= 65 && c <= 90)
-    return (1);
-  else
-    return (0);
+  if (x >= 65 && x <= 90)
+  {
+  return (1);
+  }
+  return (0);
 }
