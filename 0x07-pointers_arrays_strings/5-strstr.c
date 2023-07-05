d1c0877a76fc862e84f7fc40e88c9b6d232a7e61
Danielkipruto <dannierutto60@gmail.com>
2023-07-04 18:01:07 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,17 +11,17 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-        int i, j;
+	int i, j;
 
-        for (i = 0; haystack[i] != '\0'; i++)
-        {
-                for (j = 0; needle[j] != '\0'; j++)
-                {
-                        if (haystack[i + j] != needle[j])
-                                break;
-                }
-                if (!needle[j])
-                        return (&haystack[i]);
-        }
-        return (NULL);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
+	}
+	return (NULL);
 }
