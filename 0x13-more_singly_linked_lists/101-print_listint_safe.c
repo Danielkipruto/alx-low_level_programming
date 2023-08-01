33d59e045af8e73c6c4803d680365365f42ed941
Danielkipruto <dannierutto60@gmail.com>
2023-07-31 17:05:30 -0700

    task 2

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -81,4 +81,8 @@ size_t print_listint_safe(const listint_t *head)
 			head = head->next;
 		}
 
-		printf("-> [%p] %d\n", (void *
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
+	return (nodes);
+}
